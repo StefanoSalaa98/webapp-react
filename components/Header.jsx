d6fbf6bfc422ededeecac29f93df4a7f28077a3d
StefanoSalaa98 <sala.stefano98@gmail.com>
2025-11-03 17:47:45 +0100

    Crea l'Header della pagina

---
+++ components/Header.jsx
@@ -0,0 +1,16 @@
+// importo il router-dom per la gestione dei link
+import { Link } from "react-router-dom"
+
+// creo il componente Header
+const Header = () => {
+    return (
+        <nav className="navbar bg-body-tertiary mb-4">
+            <div className="container-fluid justify-content-start">
+                {/* se premuto torna alla Home */}
+                <Link className="navbar-brand" to="/">Movies</Link>
+            </div>
+        </nav>
+    )
+}
+
+export default Header
