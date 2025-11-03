1a9c870a920dc70c01c83ba9dca68c26812d9ec1
StefanoSalaa98 <sala.stefano98@gmail.com>
2025-11-03 17:59:07 +0100

    Aggiungi pagina di HomePage

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,15 @@
+// creo la pagina di HomePage
+const HomePage = () => {
+    return (
+        <>
+            <h1 className="text-primary">Movies</h1>
+            <div className="row row-cols-3 mt-4">
+                {/* La lista dei film verrà inserita qui */}
+                Qui ci andranno i film
+            </div>
+        </>
+
+    )
+}
+
+export default HomePage
