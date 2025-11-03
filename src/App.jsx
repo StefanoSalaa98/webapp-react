9ac37500d7949dcbb28d5d7898e66b372eeabe45
StefanoSalaa98 <sala.stefano98@gmail.com>
2025-11-03 17:48:03 +0100

    Aggiungi return() in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import './App.css'
 
 function App() {
-  <>
-    <div>
-      <h1>Benvenuti nella nostra bacheca di film</h1>
-    </div>
-  </>
+  return (
+    <>
+      <div>
+        <h1>Benvenuti nella nostra bacheca di film</h1>
+      </div>
+    </>
+  )
 }
 
 export default App
