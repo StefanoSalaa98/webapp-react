5678ee9955896182950154cdd97271b492b2ae42
StefanoSalaa98 <sala.stefano98@gmail.com>
2025-11-03 18:53:06 +0100

    Aggiungi componente card del film

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,11 +1,11 @@
 // importo il router-dom per i link
 import { Link } from "react-router-dom"
 
-// creo il componente della lista dei film
+// creo il componente card del film
 const BookCard = () => {
     return (
         <div className="card mb-4">
-            <img src="http://grupoalmuzara.com/libro/9788417954611_portada.jpg" alt="titolo film" />
+            <img src="https://c8.alamy.com/compit/2c4x052/inception-2010-diretto-da-christopher-nolan-e-con-leonardo-dicaprio-joseph-gordon-levitt-ellen-page-tom-hardy-e-ken-watanabe-una-squadra-si-rompe-al-subconscio-di-un-uomo-d-affari-usando-la-tecnologia-di-condivisione-di-sogno-per-piantare-un-seme-per-influenzare-la-sua-decisione-nel-mondo-reale-2c4x052.jpg" alt="titolo film" />
             <div className="card-body">
                 <h5 className="card-title">Titolo Film</h5>
                 <address><i>Autore</i></address>
