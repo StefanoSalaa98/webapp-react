6ff792985ead676ede5fe02e9c68f398aecf9d1f
StefanoSalaa98 <sala.stefano98@gmail.com>
2025-11-04 16:54:38 +0100

    Aggiungi chiamata axios e creazione singole movieCard

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,18 +1,39 @@
+// import axios
+import axios from "axios"
+
+// import state e effetc
+import { useState, useEffect } from "react"
+
 // importo il componente card libro
 import MovieCard from "../components/MovieCard"
 
 // creo la pagina di HomePage
 const HomePage = () => {
+
+    // variabile di stato dei film
+    const [movies, setMovies] = useState([]);
+
+    // chiamata axios per ricevere la lista dei film
+    const fecthMovies = () => {
+        axios.get('http://localhost:3000/api/movies')
+            .then(response => {
+                setMovies(response.data)
+            })
+            .catch(error => { console.log(error) })
+    }
+
+    // faccio partire la chiamata solo al primo montaggio del componente
+    useEffect(fecthMovies, []);
+
     return (
         <>
             <h1 className="text-primary">Movies</h1>
             <div className="row row-cols-3 mt-4">
-                {/* La lista dei film verrà inserita qui */}
-                <MovieCard />
-                <MovieCard />
-                <MovieCard />
-                <MovieCard />
-                <MovieCard />
+                {movies.map(movie => (
+                    <div className="col" key={movie.id}>
+                        <MovieCard movieProp={movie} />
+                    </div>
+                ))}
             </div>
         </>
 
