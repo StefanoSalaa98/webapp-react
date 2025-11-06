3d68ee5179e1106d30edb0e8de690a050a0507e9
StefanoSalaa98 <sala.stefano98@gmail.com>
2025-11-06 18:47:47 +0100

    Aggiungi componente Loader al Layout

--- src/layouts/DefaultLayout.jsx
+++ src/layouts/DefaultLayout.jsx
@@ -4,15 +4,26 @@ import { Outlet } from "react-router-dom"
 // importo il componente Header
 import Header from "../components/Header"
 
+// importo il componente loader
+import Loader from "../components/Loader"
+
+// importo hook per il contesto
+import { useGlobal } from "../contexts/globalContext"
 
 // creo componente di Layout DefaultLayout
 const DefaultLayout = () => {
+
+    // estrapolo dal context la variabile di stato
+    const { isLoading } = useGlobal();
+
     return (
         <>
             <Header />
             <main className="container">
                 <Outlet />
             </main>
+            {/* se var è true renderizza comp */}
+            {isLoading && <Loader />}
         </>
     )
 }
