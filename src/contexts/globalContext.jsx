262c7b8e3e2a01c616d1327455930a8c723d6413
StefanoSalaa98 <sala.stefano98@gmail.com>
2025-11-06 18:46:27 +0100

    Crea context

---
+++ src/contexts/globalContext.jsx
@@ -0,0 +1,32 @@
+// importo le funzionalità necessarie per il context
+import { createContext, useContext, useState } from "react";
+
+// istanza del context
+const GlobalContext = createContext();
+
+// funzione di creazione del provider
+function GlobalProvider({ children }) {
+    // variabile di stato per la gestione del loader, inizialmente settata su false
+    const [isLoading, setIsLoading] = useState(false);
+
+    return (
+        <GlobalContext.Provider
+            value={{
+                isLoading,
+                setIsLoading,
+            }}
+        >
+            {children}
+        </GlobalContext.Provider>
+    );
+}
+
+
+// Definisco un hook per il context
+function useGlobal() {
+    const context = useContext(GlobalContext);
+    return context;
+}
+
+// Esporto il nostro provider ed il nostro hook
+export { GlobalProvider, useGlobal }
